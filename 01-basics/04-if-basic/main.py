c38ae70c349e7ed45194e387b0862145fac7f47c
askl4267 <askl4267@gmail.com>
2026-03-04 16:00:21 +0900

    04の回答

--- 01-basics/04-if-basic/main.py
+++ 01-basics/04-if-basic/main.py
@@ -2,13 +2,25 @@
 # ageが18以上のときに"Adult"を表示する
 age = 20
 # ここにコードを書いてください
+if age >= 18:
+    print("Adult")
 
 # Q02: If-Else
 # ageが18以上のときに"Adult"、それ以外のときに"Minor"を表示する
 age = 20
 # ここにコードを書いてください
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
 
 # Q03: If-Elif-Else
 # ageに応じて"Child"、"Teen"、"Adult"を表示する
 age = 15
 # ここにコードを書いてください
+if age >= 18:
+    print("Adult")
+elif age < 10:
+    print("Child")
+else:
+    print("Teen")
