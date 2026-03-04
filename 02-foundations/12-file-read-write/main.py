4eb53f2720d3151aa757756ac34d61b5e928ce32
askl4267 <askl4267@gmail.com>
2026-03-04 22:59:40 +0900

    12の回答

--- 02-foundations/12-file-read-write/main.py
+++ 02-foundations/12-file-read-write/main.py
@@ -1,11 +1,21 @@
 # Q01: File Write
 # "output.txt" という名前のファイルに "Hello, Python!" という文字列を書き込んでください。文字コードはutf-8を使用してください。
 # ここにコードを書いてください
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write("Hello, Python!")
 
 # Q02: File Read
 # "output.txt" ファイルから内容を読み込んで、その内容を表示してください。文字コードはutf-8を使用してください。
 # ここにコードを書いてください
+with open("output.txt", "r", encoding="utf-8") as f:
+    print(f.read())
+
 
 # Q03: File Append
 # "output.txt" ファイルに改行してから "This is a new line." という文字列を追記してください。その後、ファイル全体の内容を読み込んで表示してください。
 # ここにコードを書いてください
+with open("output.txt", "a", encoding="utf-8") as f:
+    f.write("\nThis is a new line.")
+
+with open("output.txt", "r", encoding="utf-8") as f:
+    print(f.read())
