22dcdf15b44884a7e6899fc70c6407664491b3e1
askl4267 <askl4267@gmail.com>
2026-03-04 16:01:10 +0900

    10の回答

--- 01-basics/10-string-basic/main.py
+++ 01-basics/10-string-basic/main.py
@@ -2,13 +2,15 @@
 # "apple,banana,orange"という文字列をカンマで分割してリストを作成し、表示してください。
 text = "apple,banana,orange"
 # ここにコードを書いてください
+print(text.split(","))
 
 # Q02: String Join
 # ["Hello", "World", "Python"] というリストの要素をスペースで結合して、"Hello World Python" という文字列を作成し、表示してください。
 words = ["Hello", "World", "Python"]
 # ここにコードを書いてください
+print(" ".join(words))
 
 # Q03: Split and Join
 # "one-two-three"という文字列をハイフンで分割し、アンダースコアで結合して "one_two_three" という文字列を作成し、表示してください。
 sentence = "one-two-three"
-# ここにコードを書いてください
+print("_".join(sentence.split("-")))
