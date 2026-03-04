ddb5a1964654ba47c4f915d1a1177cd5609457b5
askl4267 <askl4267@gmail.com>
2026-03-04 16:00:44 +0900

    07の回答

--- 01-basics/07-functions-basic/main.py
+++ 01-basics/07-functions-basic/main.py
@@ -1,23 +1,24 @@
 # 問１：「Hello Python」を３回表示する関数
 def print_hello_python():
     # 以下に関数内処理を記述してください
-    pass
+    for _ in range(3):
+        print("Hello Python")
 
 # 関数を実行してください
-
+print_hello_python()
 
 ### 問２：引数を受け取って表示する関数
 def say_hello(name):
     # 以下に関数内処理を記述してください
-    pass
+    print(f"Hello {name}")
 
 # 関数を実行してください
-
+say_hello("Alice")
 
 # 問３：戻り値のある関数
 def format_name(name):
     # 以下に関数内処理を記述してください
-    pass
+    return "Hello " + name
 
 # 関数を実行してください
-
+print(format_name("太郎"))
