4d9c5ab7c7af2cdeab6f433091fb41fdd637a4b7
askl4267 <askl4267@gmail.com>
2026-03-04 16:00:36 +0900

    06の回答

--- 01-basics/06-while-basic/main.py
+++ 01-basics/06-while-basic/main.py
@@ -1,11 +1,25 @@
 # Q01: While Basic
 # 1から3まで数値を表示する
 # ここにコードを書いてください
+counter = 1
+while counter <= 3:
+    print(counter)
+    counter += 1
+
 
 # Q02: While Counter
 # 0から4まで数値を表示する
 # ここにコードを書いてください
+counter = 0
+while counter <= 4:
+    print(counter)
+    counter += 1
+
 
 # Q03: While Condition
 # "Hello"を3回表示する
 # ここにコードを書いてください
+counter = 0
+while counter < 3:
+    print("Hello")
+    counter += 1
