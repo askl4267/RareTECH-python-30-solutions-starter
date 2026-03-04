59176da94995a05c540ca5a41c65ac086851ea82
askl4267 <askl4267@gmail.com>
2026-03-04 22:59:57 +0900

    14の回答

--- 02-foundations/14-lambda-map-filter/main.py
+++ 02-foundations/14-lambda-map-filter/main.py
@@ -3,10 +3,13 @@
 # `map` と `lambda` を使用してください。
 numbers_q1 = [1, 2, 3, 4, 5]
 # ここにコードを書いてください
-
+squared_numbers = list(map(lambda num: num ** 2, numbers_q1))
+print(squared_numbers)
 
 # Q2: `filter` と `lambda` で条件に合う要素を抽出する
 # `numbers_q2` から偶数のみを抽出した新しいリスト `even_numbers` を作成し、表示してください。
 # `filter` と `lambda` を使用してください。
 numbers_q2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # ここにコードを書いてください
+even_numbers = list(filter(lambda num: num % 2 == 0, numbers_q2))
+print(even_numbers)
