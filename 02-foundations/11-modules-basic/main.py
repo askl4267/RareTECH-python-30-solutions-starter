613b7aad25038d728bb9df81d0012a9dac2dbe89
askl4267 <askl4267@gmail.com>
2026-03-04 16:01:20 +0900

    11の回答

--- 02-foundations/11-modules-basic/main.py
+++ 02-foundations/11-modules-basic/main.py
@@ -1,18 +1,23 @@
 # 問１：標準ライブラリをimportしよう
 # mathライブラリをインポートし、math.sqrt() を使って25の平方根を計算し、結果を "25の平方根は...です" の形式で表示してください。
 # 以下に処理を記述してください
-
+import math
+print(f"25の平方根は{math.sqrt(25)}です")
 
 # 問２：モジュール内の特定のプログラムをimportしよう
 # mathライブラリからpiをインポートし、"円周率は...です" の形式で表示してください。
 # 以下に処理を記述してください
-
+from math import pi
+print(f"円周率は{pi}です")
 
 # 問３：asを使って別名をつけてみよう
 # randomライブラリをrという別名でインポートし、r.randint(1, 6) を使って1から6までのランダムな整数を生成して表示してください。
 # 以下に処理を記述してください
-
+import random as r
+print(r.randint(1, 6))
 
 # 問４：自作モジュールを作成しよう
 # greet.pyモジュールをインポートし、greet.hello() 関数を "Python" という引数で呼び出してください。
 # 以下に処理を記述してください
+import greet
+greet.hello("Python")
